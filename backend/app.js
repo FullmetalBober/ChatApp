294ee8e063c40d8fa80541502c666ecc7f45ebdc
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2023-04-22 13:34:37 +0300

    security setup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController.js');
@@ -10,8 +17,35 @@ const messageRouter = require('./routes/messageRoutes');
 
 const app = express();
 
+app.enable('trust proxy');
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: ['search'],
+  })
+);
+
+app.use(compression());
 
 app.use('/api/users', userRouter);
 app.use('/api/chats', chatRouter);
